3d8a51fbf22d05c9261eff5a05b3c31da762194f
DeepKolos <k1206521365k@126.com>
2017-11-25 12:26:00 +0800

    蹭课增加老师搜索

--- modules/auditor/controllers/HomeCtrl.php
+++ modules/auditor/controllers/HomeCtrl.php
@@ -2,12 +2,26 @@
 
 class HomeCtrl extends BaseCtrl {
   public function actionSearchCourse(){
-    if(isset($_GET['keyWord'])){
+    if(isset($_GET['keyWord']) && isset($_GET['type'])){
       $keyword = $_GET['keyWord'];
-      echo json_encode(
-        $this->edu_major_listModel->findAll('`name` LIKE ?',
-          array("%{$keyword}%"))
-      );
+      $type = $_GET['type'];
+
+      //class的话先模糊搜索,再查看详细
+      if($type === 'class')
+        echo json_encode(
+          $this->edu_major_listModel->findAll('`name` LIKE ?',
+            array("%{$keyword}%"))
+        );
+
+      //teacher就输出这个老师的课程就是了
+      elseif ($type === 'teacher') {
+        echo json_encode(
+          $this->edu_major_infoModel->findAll('`teacher` = ?',
+            array($keyword))
+        );
+      }
+
+      
     }
   }
   public function actionCourseDetial(){
--- modules/auditor/views/Home/controller.js
+++ modules/auditor/views/Home/controller.js
@@ -8,8 +8,8 @@ exports.actionIndex = function() {
 
 }
 
-exports.viewUpdateMatchList = function() {
-  var $inputKeyword = document.querySelector('.inputKeyword input');
+exports.viewUpdateMatchList = function(params) {
+  var $inputKeyword = document.querySelector('.inputKeyword');
 
   css($$('display_result')[0], {
     'opacity': 0,
@@ -25,34 +25,48 @@ exports.viewUpdateMatchList = function() {
       method: 'get',
       url: this.Router.url('Home', 'SearchCourse'),
       arg: {
-        keyWord: $inputKeyword.value
+        keyWord: $inputKeyword.value,
+        type: params.type
       },
       func: function(rep) {
         var result = JSON.parse(rep);
         var display_result = $$('display_result')[0];
         if (result) {
-          if (result.length > 0) {
-            var list = strToDom('\
-                        <ul class="possible_options"></ul>\
-                        ');
-            for (var i = 0; i < result.length; i++) {
-              var li = strToDom('\
-                            <li style="display: block;"><a href="#Home/CourseDetial?id=' + result[i].id + '">' + result[i].name + '</a></li>\
-                            ');
-              css(li, {
-                'transform': 'translateY(50px)',
-                'transition-delay': (i / 28) + 's',
-              });
-              list.appendChild(li);
+
+          if(params.type === 'class'){
+            if (result.length > 0) {
+              var list = strToDom('\
+                          <ul class="possible_options"></ul>\
+                          ');
+              for (var i = 0; i < result.length; i++) {
+                var li = strToDom('\
+                              <li style="display: block;"><a href="#Home/CourseDetial?id=' + result[i].id + '">' + result[i].name + '</a></li>\
+                              ');
+                css(li, {
+                  'transform': 'translateY(50px)',
+                  'transition-delay': (i / 28) + 's',
+                });
+                list.appendChild(li);
+              }
+            } else {
+              var list = strToDom('\
+                          <div style="text-align:center;margin-top:30px;">找不到~</div>\
+                          ');
             }
-          } else {
-            var list = strToDom('\
-                        <div style="text-align:center;margin-top:30px;">找不到~</div>\
-                        ');
+
+          }else if(params.type === 'teacher'){
+            var list = getDom_courseTable(rep);
+            
           }
+
           setTimeout(function() {
-            display_result.innerHTML = '';
-            display_result.appendChild(list);
+            
+            if(typeof list === 'string')
+              display_result.innerHTML = list;
+            else{
+              display_result.innerHTML = '';
+              display_result.appendChild(list);
+            }
             css(display_result, {
               'opacity': 1,
               'transform': 'translateY(0px)'
@@ -83,9 +97,6 @@ exports.viewUpdateMatchList = function() {
 };
 
 exports.actionCourseDetial = function(args) {
-  var weekDay = ['一', '二', '三', '四', '五', '六', '日'];
-  var weekly = ['', '单', '双'];
-
   this.Page.load({
     method: 'get',
     url: this.Router.url(),
@@ -93,68 +104,7 @@ exports.actionCourseDetial = function(args) {
       id: args.id
     },
     func: function(rep) {
-      //显示课程数据
-      var result = JSON.parse(rep);
-      //首行
-      var table = strToDom('\
-            <div class="display_result">\
-            <table border="1" bordercolor="#e0e0e0">\
-                <tbody>\
-                </tbody>\
-            </table>\
-            <a href="#back"><div class="btnOneLine">返回</div></a>\
-            </div>\
-            ');
-      var trs = '\
-            <tr>\
-                <td>课程</td>\
-                <td>老师</td>\
-                <td>周次</td>\
-                <td>节次</td>\
-                <td>课室</td>\
-            </tr>\
-            ';
-      for (var i = 0; i < result.length; i++) {
-        var section = '星期' + weekDay[result[i]['weekDay']] + '<br>[' + result[i]['sectionStart'] + '-' + result[i]['sectionEnd'] + ']<br>' + weekly[result[i]['weekly']];
-        var teacher = result[i]['teacher'].split(' ');
-        teacher = teacher.join('<br>');
-        teacher = teacher.replace(/（(.+)）/g, '<br>$1');
-        var week_have_class = shorten_week_has_class(result[i]['weekHaveClass']);
-        var name = result[i]['name'];
-        trs += '<tr style="transform:translateY(50px);transition-delay:' + (i / 26) + 's!important;">';
-        if (name.length > 7) {
-          var fornt_major_name = name.slice(0, name.length / 2);
-          var back_major_name = name.slice(name.length / 2, name.length);
-          name = fornt_major_name + '<br>' + back_major_name;
-        }
-        var place_name_full = result[i]['room'].replace(/（(.+)）/g, '<br>$1');
-        // 山根祥子（ShokoYamane）
-        week_have_class = week_have_class.join('<br>');
-        trs += '<td>' + name + '</td>';
-        trs += '<td>' + teacher + '</td>';
-        trs += '<td>' + week_have_class + '</td>';
-        trs += '<td>' + section + '</td>';
-        trs += '<td>' + place_name_full + '</td>';
-        trs += '</tr>';
-      }
-      table.getElementsByTagName('tbody')[0].innerHTML = trs;
-
-      setTimeout(function() {
-        //tr animation
-        var tr = document.querySelectorAll('.display_result tr');
-        for (var i = 0; i < tr.length; i++) {
-          tr[i].style.transform = 'translateY(0px)';
-        }
-      }, 400);
-      if (result.length != 0)
-        return table.outerHTML;
-      else {
-        return strToDom('\
-                <div><div style="height:1px;"></div>\
-                <div class="messageEmpty">没有数据~</div>\
-                <a href="#back"><div class="btnOneLine">返回</div></a>\
-                <div>').outerHTML;
-      }
+      return getDom_courseTable(rep);
     }
   });
 };
@@ -180,4 +130,71 @@ function shorten_week_has_class(str) {
   else
     result.push(first.toString());
   return result;
+}
+
+function getDom_courseTable(rep){
+  var weekDay = ['一', '二', '三', '四', '五', '六', '日'];
+  var weekly = ['', '单', '双'];
+  //显示课程数据
+  var result = JSON.parse(rep);
+  //首行
+  var table = strToDom('\
+        <div class="display_result">\
+        <table border="1" bordercolor="#e0e0e0">\
+            <tbody>\
+            </tbody>\
+        </table>\
+        <a href="#back"><div class="btnOneLine">返回</div></a>\
+        </div>\
+        ');
+  var trs = '\
+        <tr>\
+            <td>课程</td>\
+            <td>老师</td>\
+            <td>周次</td>\
+            <td>节次</td>\
+            <td>课室</td>\
+        </tr>\
+        ';
+  for (var i = 0; i < result.length; i++) {
+    var section = '星期' + weekDay[result[i]['weekDay']] + '<br>[' + result[i]['sectionStart'] + '-' + result[i]['sectionEnd'] + ']<br>' + weekly[result[i]['weekly']];
+    var teacher = result[i]['teacher'].split(' ');
+    teacher = teacher.join('<br>');
+    teacher = teacher.replace(/（(.+)）/g, '<br>$1');
+    var week_have_class = shorten_week_has_class(result[i]['weekHaveClass']);
+    var name = result[i]['name'];
+    trs += '<tr style="transform:translateY(50px);transition-delay:' + (i / 26) + 's!important;">';
+    if (name.length > 7) {
+      var fornt_major_name = name.slice(0, name.length / 2);
+      var back_major_name = name.slice(name.length / 2, name.length);
+      name = fornt_major_name + '<br>' + back_major_name;
+    }
+    var place_name_full = result[i]['room'].replace(/（(.+)）/g, '<br>$1');
+    // 山根祥子（ShokoYamane）
+    week_have_class = week_have_class.join('<br>');
+    trs += '<td>' + name + '</td>';
+    trs += '<td>' + teacher + '</td>';
+    trs += '<td>' + week_have_class + '</td>';
+    trs += '<td>' + section + '</td>';
+    trs += '<td>' + place_name_full + '</td>';
+    trs += '</tr>';
+  }
+  table.getElementsByTagName('tbody')[0].innerHTML = trs;
+
+  setTimeout(function() {
+    //tr animation
+    var tr = document.querySelectorAll('.display_result tr');
+    for (var i = 0; i < tr.length; i++) {
+      tr[i].style.transform = 'translateY(0px)';
+    }
+  }, 400);
+  if (result.length != 0)
+    return table.outerHTML;
+  else {
+    return strToDom('\
+            <div><div style="height:1px;"></div>\
+            <div class="messageEmpty">没有数据~</div>\
+            <a href="#back"><div class="btnOneLine">返回</div></a>\
+            <div>').outerHTML;
+  }
 }
