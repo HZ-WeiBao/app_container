29882640fdcd854a1dc1c50c92a25f5bfb66068f
DeepKolos <k1206521365k@126.com>
2017-12-04 19:49:17 +0800

    空课室数据补丁

---
+++ patch.sql
@@ -0,0 +1,1 @@
+INSERT INTO `edu_classroom_course` (`id`, `name`, `teacher`, `weekHaveClass`, `weekly`, `weekDay`, `sectionStart`, `sectionEnd`, `room`, `building`, `class`) VALUES ('0800087', '土力学与地基基础', '赵熙', '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19', '0', '0', '7', '8', '1-211', '1', '15土木工程1班 15土木工程2班');
